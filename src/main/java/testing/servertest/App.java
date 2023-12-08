f9a4de75c8079170d9cbf8541249dfa8c2a860a2
Ranjana1984 <142005449+Ranjana1984@users.noreply.github.com>
2023-12-08 16:37:55 +0530

    test code added

--- src/main/java/testing/servertest/App.java
+++ src/main/java/testing/servertest/App.java
@@ -25,37 +25,60 @@ public class App
     //setting properties
     WebDriverManager.chromedriver().setup();
     ChromeOptions chromeOptions = new ChromeOptions();
-	  chromeOptions.addArguments("--headless");
+	  //chromeOptions.addArguments("--headless");
 	  //chromeOptions.addArguments("--disable-gpu");
 	 //chromeOptions.addArguments("--no-sandbox");
     //open url
     System.out.println("Driver opening up the url in browser");	
     WebDriver driver = new ChromeDriver(chromeOptions);
-    driver.get("http://54.215.79.169:8083/contact.html");	
+    driver.get("http://localhost:8081/contact.html");	
+   // driver.get("https://www.facebook.com");
+    
     //invole implicit waits to load the page
     driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    
     System.out.println("Enter details in the form");
     //enter details
-    driver.findElement(By.id("inputName")).sendKeys("Laxmi");
+    //input name
+    driver.findElement(By.id("inputName")).sendKeys("Sam");
+    Thread.sleep(3000);
+    driver.findElement(By.id("inputNumber")).sendKeys("999999999");
     Thread.sleep(1000);
-    driver.findElement(By.id("inputNumber")).sendKeys("900000009");
+    driver.findElement(By.id("inputMail")).sendKeys("sam@xyz.com");
     Thread.sleep(1000);
-    driver.findElement(By.id("inputMail")).sendKeys("Laxmi@xyz.com");
+    driver.findElement(By.id("inputMessage")).sendKeys("Hi, I am interested in insurance");
     Thread.sleep(1000);
-    driver.findElement(By.id("inputMessage")).sendKeys("Hi, Iam intrested in insurance");
-    Thread.sleep(1000);
+    
     driver.findElement(By.id("my-button")).click();
-    Thread.sleep(1000);
-    String response = driver.findElement(By.id("response")).getText();
-    System.out.println(response);
-    Thread.sleep(1000);
-    System.out.println("test scripts are executed");
+    
+//    String response = driver.findElement(By.id("response")).getText();
+   
+//    System.out.println(response);
+    
+    System.out.println("test scripts executed");
+    
     TakesScreenshot scrShot = ((TakesScreenshot)driver);
+    
     File srcFile = scrShot.getScreenshotAs(OutputType.FILE);
-    File destFile = new File("/var/lib/jenkins/workspace/capstone-project-test/Testss.jpg");
+    
+    File destFile = new File("/Users/jivan/test-reports.jpg");
+    //File destFile = new File("/var/lib/jenkins/workspace/insure-me-test-scripts/test-reports.jpg");
+    
+    
     FileUtils.copyFile(srcFile, destFile);
+    
     Thread.sleep(1000);
-    System.out.println("ScreenShot Taken");
+    
+    
+    
+    
     driver.quit();
+    
+       	
+    
+    	
+        System.out.println( "Hello World!" );
     }
 }
+    
+ 
